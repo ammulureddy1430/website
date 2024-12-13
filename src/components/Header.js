e609f51a491fcb614628a0801305d6428f1a0b14
ammulureddy1722 <ammulureddy1722@gmail.com>
2024-12-13 21:23:05 +0530

    added some content

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import Typewriter from "typewriter-effect"
 export const Header = (props) => {
   return (
     <header id="header">
@@ -11,16 +11,39 @@ export const Header = (props) => {
                 <h1 className="heading-title">
                   {props.data ? props.data.title : "Loading"}
                   <span></span>
-                  <span className="main-element theme-color"></span>
-                  <span className="typed-cursor"></span>
+                  <span className="main-element theme-color">
+                    <Typewriter className="typewriter"
+                      onInit={(typewriter) => {
+                          typewriter
+                              .typeString("Web Developers")
+                              .pauseFor(1000)
+                              .deleteAll()
+                              .typeString("Creative")
+                              .pauseFor(1000)
+                              .deleteAll()
+                              .typeString("Ambitious")
+                              .start();      
+                      }} 
+                    />
+                  </span>
                 </h1>
+                
                 <p>{props.data ? props.data.paragraph : "Loading"}</p>
-                <a
-                  href="#features"
-                  className="btn btn-custom btn-lg page-scroll"
-                >
-                  Get Started
-                </a>{" "}
+                <div className="btn-bar">
+                  <a
+                    href="#features"
+                    className="btn btn-custom btn-lg page-scroll"
+                  >
+                    Get Started
+                  </a>{" "}
+                  <a 
+                    href="#" 
+                    className="btn btn-custom-outline"
+                  >
+                    Contact Us
+                  </a>
+                </div>
+                
               </div>
             </div>
           </div>
